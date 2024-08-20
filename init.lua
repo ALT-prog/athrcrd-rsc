dfe0adafc9d1da8bcf4c3f1b0ef7d55785767886
ALT-prog <79774760+ALT-prog@users.noreply.github.com>
2024-08-20 15:24:50 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,31 @@
+version = 108
+writefile = (function()end)
+readfile = (function()end)
+
+if (get_current_version() ~= version) then
+    lua_exit()
+end
+
+local function SecureInitialize()
+    local _get_current_version = get_current_version
+    get_current_version = (function()
+        return _get_current_version()
+    end)
+end
+SecureInitialize()
+SecureInitialize = nil
+lua_exit = nil
+
+os.exit = (function()
+    return error("Missing permissions")
+end)
+os.rename = (function()
+    return error("Missing permissions")
+end)
+os.remove = (function()
+    return error("Missing permissions")
+end)
+package.loadlib = (function()
+    return error("Missing permissions")
+end)
 
